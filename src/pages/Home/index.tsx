d20ad29af7508061880210dc1303f55a699565ad
Ticianne <ticidias@gmail.com>
2022-07-27 22:23:01 -0300

    Feat: início do localstorage para os filtros

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,23 @@
-import BarChart from "../../dashboards/BarCharts";
-import { AppContainer } from "../../style/CommunsStyle";
-import AreaCharts from "../../dashboards/AreaCharts";
-import LineChart from "../../dashboards/LineCharts";
-import MixedContainer from "../../dashboards/MixedContainer";
-import Filter from "../../components/Filter";
-import PieChartsCountry from "../../dashboards/PieChartsCountry";
-import Title from "../../components/Title";
-import { useState } from "react";
-import { LabelContainer } from "./style";
+import BarChart from '../../dashboards/BarCharts';
+import { AppContainer } from '../../style/CommunsStyle';
+import AreaCharts from '../../dashboards/AreaCharts';
+import LineChart from '../../dashboards/LineCharts';
+import MixedContainer from '../../dashboards/MixedContainer';
+import Filter from '../../components/Filter';
+import PieChartsCountry from '../../dashboards/PieChartsCountry';
+import Title from '../../components/Title';
+import { useEffect, useState } from 'react';
+import { LabelContainer } from './style';
 
 const Home = () => {
+  const [show, setShow] = useState({
+    filter: false,
+    area: true,
+    bar: true,
+    line: true,
+    pie: true,
+    mixed: true
+  });
   const [filter, setFilter] = useState(false);
   const [showArea, setShowArea] = useState(true);
   const [showBar, setShowBar] = useState(true);
@@ -17,24 +25,75 @@ const Home = () => {
   const [showPie, setShowPie] = useState(true);
   const [showMixed, setShowMixed] = useState(true);
 
+  let dashboard = [];
+
+  if (localStorage) {
+    let dashboard = localStorage.getItem('dashboard');
+  }
+
   const handleClick = () => {
-    setFilter((visible) => !visible);
+    setFilter(visible => !visible);
   };
   const handleClickArea = () => {
-    setShowArea((visible) => !visible);
+    setShowArea(visible => !visible);
+    setShow(prev => ({
+      ...prev,
+      area: !showArea
+    }));
+    localStorage.setItem('dashboard', JSON.stringify(show));
   };
   const handleClickBar = () => {
-    setShowBar((visible) => !visible);
+    setShowBar(visible => !visible);
+    setShow(prev => ({
+      ...prev,
+      bar: !showBar
+    }));
+    localStorage.setItem('dashboard', JSON.stringify(show));
   };
   const handleClickLine = () => {
-    setShowLine((visible) => !visible);
+    setShowLine(visible => !visible);
+    setShow(prev => ({
+      ...prev,
+      line: !showLine
+    }));
+    localStorage.setItem('dashboard', JSON.stringify(show));
   };
   const handleClickPie = () => {
-    setShowPie((visible) => !visible);
+    setShowPie(visible => !visible);
+    setShow(prev => ({
+      ...prev,
+      pie: !showPie
+    }));
+    localStorage.setItem('dashboard', JSON.stringify(show));
   };
   const handleClickMixed = () => {
-    setShowMixed((visible) => !visible);
+    setShowMixed(visible => !visible);
+    setShow(prev => ({
+      ...prev,
+      mixed: !showMixed
+    }));
+    localStorage.setItem('dashboard', JSON.stringify(show));
   };
+
+  type newShow = {
+    show: object;
+    area: boolean;
+    bar: boolean;
+    line: boolean;
+    pie: boolean;
+    mixed: boolean;
+    filter: boolean;
+  };
+
+  useEffect(() => {
+    const show: newShow = JSON.parse(localStorage.getItem('dashboard') || '{}');
+    setShowArea(show.area);
+    setShowBar(show.bar);
+    setShowLine(show.line);
+    setShowPie(show.pie);
+    setShowMixed(show.mixed);
+    console.log(show);
+  }, []);
 
   return (
     <>
